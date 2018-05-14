8941cc52aa2e38b1545959016dfc663c31d002ba
Marko <marko.tursic6@gmail.com>
2018-05-14 18:43:56 +0200

    Trader logic finish => testing

--- exchange/trader.py
+++ exchange/trader.py
@@ -5,13 +5,16 @@ from exchange.models import ActiveOrder, User
 
 
 def run_trader():
-
     buy_orders = DBSession.query(ActiveOrder).filter_by(type=ActiveOrder.BUY_ORDER, deleted=False) \
         .order_by(desc(ActiveOrder.price), ActiveOrder.time)
 
+    if buy_orders.first() is None:
+        print('No buy orders')
+        return
+
     max_buy_price = buy_orders.first().price
 
-    sell_orders = DBSession.query(ActiveOrder).filter(ActiveOrder.price <= max_buy_price)\
+    sell_orders = DBSession.query(ActiveOrder).filter(ActiveOrder.price <= max_buy_price) \
         .filter_by(type=ActiveOrder.SELL_ORDER, deleted=False).order_by(ActiveOrder.price, ActiveOrder.time)
 
     if sell_orders.first():
@@ -42,7 +45,7 @@ def run_trader():
                 buy_amount = buy_order.amount
                 buy_user_id = buy_order.user_id
 
-                if sell_user_id != buy_user_id:
+                if sell_user_id == buy_user_id:
                     print('Same user trade WARNING!')
                     break
 
@@ -61,28 +64,52 @@ def run_trader():
                             BTC: + buy_amount - sell_amount
                             EUR: - (buy_amount - sell_amount) * buy_price
                         """
-
                         DBSession.query(ActiveOrder).filter_by(id=sell_order_id).delete()
-                        sell_user = DBSession.query(User).filter_by(id=sell_user_id)
-                        DBSession.query(User).filter_by(id=sell_user_id)\
+                        sell_user = DBSession.query(User).filter_by(id=sell_user_id).one()
+                        DBSession.query(User).filter_by(id=sell_user_id) \
                             .update({'eur': sell_user.eur + (buy_price * sell_amount),
                                      'btc': sell_user.btc - sell_amount})
 
-                        buy_order -= sell_amount
-                        DBSession.query(ActiveOrder).filter_by(id=buy_order_id).update({'amount': buy_order})
-                        buy_user = DBSession.query(User).filter_by(id=buy_user_id)
+                        # buy_amount -= sell_amount
+                        update_amount = buy_amount - sell_amount
+                        DBSession.query(ActiveOrder).filter_by(id=buy_order_id).update({'amount': update_amount})
+                        buy_user = DBSession.query(User).filter_by(id=buy_user_id).one()
                         DBSession.query(User).filter_by(id=buy_user_id) \
-                            .update({'eur': buy_user.eur - (buy_amount - sell_amount) * buy_price,
+                            .update({'eur': buy_user.eur - sell_amount * buy_price,
                                      'btc': buy_user.btc + sell_amount})
+                        break
+
+                    if sell_amount == buy_amount:
+                        """
+                        """
+                        DBSession.query(ActiveOrder).filter_by(id=sell_order_id).delete()
+                        sell_user = DBSession.query(User).filter_by(id=sell_user_id).one()
+                        DBSession.query(User).filter_by(id=sell_user_id) \
+                            .update({'eur': sell_user.eur + (sell_price * sell_amount),
+                                     'btc': sell_user.btc - sell_amount})
+
+                        DBSession.query(ActiveOrder).filter_by(id=buy_order_id).delete()
+                        buy_user = DBSession.query(User).filter_by(id=buy_user_id).one()
+                        DBSession.query(User).filter_by(id=buy_user_id) \
+                            .update({'eur': buy_user.eur - buy_amount * buy_price,
+                                     'btc': buy_user.btc + buy_amount})
                         break
 
                     if sell_amount > buy_amount:
                         """
-                        Sell order updated:
-                            amount: sell_amount - buy_amount
-                        Sell user balance:
-                            BTC: 
                         """
+                        update_amount = sell_amount - buy_amount
+                        DBSession.query(ActiveOrder).filter_by(id=sell_order_id).update({'amount': update_amount})
+                        sell_user = DBSession.query(User).filter_by(id=sell_user_id).one()
+                        DBSession.query(User).filter_by(id=sell_user_id) \
+                            .update({'eur': sell_user.eur + (sell_price * sell_amount),
+                                     'btc': sell_user.btc - sell_amount})
+
+                        DBSession.query(ActiveOrder).filter_by(id=buy_order_id).delete()
+                        buy_user = DBSession.query(User).filter_by(id=buy_user_id).one()
+                        DBSession.query(User).filter_by(id=buy_user_id) \
+                            .update({'eur': buy_user.eur - sell_amount * buy_price,
+                                     'btc': buy_user.btc + sell_amount})
 
 
 run_trader()
